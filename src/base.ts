65376f90d387c244f4c89c2840210c26ada185bb
Matthieu Symoens <matthieu.symoens@gmail.com>
2025-03-27 22:15:51 -0400

    Fixed linting error

--- src/base.ts
+++ src/base.ts
@@ -1,9 +1,9 @@
 import stylisticPlugin from '@stylistic/eslint-plugin'
 import importPlugin from 'eslint-plugin-import'
+import { getGlobals } from './getGlobals.js'
 import baseRules from './rules/base.js'
 import importRules from './rules/import.js'
 import stylisticRules from './rules/stylistic.js'
-import { getGlobals } from './getGlobals.js'
 import type { Globals } from './Globals.js'
 
 const baseConfig = (globals: Globals | undefined): any => {
